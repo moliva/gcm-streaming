e9411d177a78be596ab2eb0e692809005a458849
Miguel <oliva.miguelh@gmail.com>
2015-06-08 15:01:59 +0200

    Implemented sliding windows with time based eviction and trigger policies

--- src/main/java/org/inria/scale/streams/windows/sliding/TimeEvictionStrategy.java
+++ src/main/java/org/inria/scale/streams/windows/sliding/TimeEvictionStrategy.java
@@ -1,13 +1,29 @@
 package org.inria.scale.streams.windows.sliding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.inria.scale.streams.operators.Window;
 import org.javatuples.Tuple;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
+
 public class TimeEvictionStrategy implements EvictionStrategy {
 
+	private static final int DEFAULT_INTERVAL = 100;
+
 	private final long milliseconds;
+
+	private Window window;
+	private Timer timer;
+
+	private final Map<Tuple, Long> times = new HashMap<>();
 
 	public TimeEvictionStrategy(final long milliseconds) {
 		this.milliseconds = milliseconds;
@@ -15,20 +31,52 @@ public class TimeEvictionStrategy implements EvictionStrategy {
 
 	@Override
 	public void initialize(final Window window) {
-		// TODO Auto-generated method stub
-		
+		this.window = window;
+		initializeTimers();
 	}
 
-	@Override
-	public void tearDown() {
-		// TODO Auto-generated method stub
-		
+	private void initializeTimers() {
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+
+			@Override
+			public void run() {
+				singleCheck();
+			}
+		}, DEFAULT_INTERVAL, DEFAULT_INTERVAL);
 	}
 
 	@Override
 	public void check(final List<Tuple> tuples) {
-		// TODO Auto-generated method stub
-		
+		final long currentTime = System.currentTimeMillis();
+
+		final Queue<Tuple> queue = window.getTuplesQueue();
+		for (final Tuple tuple : tuples) {
+			times.put(tuple, currentTime);
+		}
+		queue.addAll(tuples);
+	}
+
+	private void singleCheck() {
+		final long currentTime = System.currentTimeMillis();
+
+		final Queue<Tuple> tuplesQueue = window.getTuplesQueue();
+		final List<Tuple> tuplesToRemove = FluentIterable.from(new ArrayList<>(tuplesQueue)).filter(new Predicate<Tuple>() {
+
+			@Override
+			public boolean apply(final Tuple tuple) {
+				return currentTime - times.get(tuple) > milliseconds;
+			}
+		}).toList();
+
+		tuplesQueue.removeAll(tuplesToRemove);
+	}
+
+	@Override
+	public void tearDown() {
+		if (timer != null) {
+			timer.cancel();
+		}
 	}
 
 }
