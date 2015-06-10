3a19f6c0f29c745e186a911c1e2ef5e46bea5fd3
Miguel <oliva.miguelh@gmail.com>
2015-06-10 11:33:52 +0200

    Removing setter for queues in window operator

--- src/main/java/org/inria/scale/streams/operators/Window.java
+++ src/main/java/org/inria/scale/streams/operators/Window.java
@@ -16,7 +16,7 @@ import org.objectweb.proactive.multiactivity.MultiActiveService;
 
 public class Window extends MulticastInStreamBindingController implements InStream, WindowConfiguration, RunActive {
 
-	private Queue<Tuple> tuples = new ConcurrentLinkedQueue<>();
+	private final Queue<Tuple> tuples = new ConcurrentLinkedQueue<>();
 
 	private String windowConfigurationJson;
 	private WindowStrategy windowStrategy;
@@ -81,10 +81,6 @@ public class Window extends MulticastInStreamBindingController implements InStre
 
 	public Queue<Tuple> getTuplesQueue() {
 		return tuples;
-	}
-
-	public void setTuplesQueue(final Queue<Tuple> tuples) {
-		this.tuples = tuples;
 	}
 
 }
