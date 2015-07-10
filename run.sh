f80228dc740930c88fe378b214416832913cd6db
Miguel <oliva.miguelh@gmail.com>
2015-07-10 15:08:25 +0200

    Deleted deprecated file

--- run.sh
+++
@@ -1,1 +0,0 @@
-mvn exec:java -Dexec.mainClass=org.inria.scale.streams.Main -Djava.security.manager -Djava.security.policy=src/test/resources/allPerm.policy  -Dgcm.provider=org.objectweb.proactive.core.component.Fractive
